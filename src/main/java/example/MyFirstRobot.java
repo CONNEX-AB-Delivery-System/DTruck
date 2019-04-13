86b41698c8f41a5ecc0a6e6def4d8c48315ea0b5
Ehsan Zabardast <ehsanzabardast@gmail.com>
2019-04-13 17:17:57 +0200

    linereader fix

--- src/main/java/example/MyFirstRobot.java
+++ src/main/java/example/MyFirstRobot.java
@@ -25,12 +25,17 @@ public class MyFirstRobot {
         final EV3MediumRegulatedMotor motorDrive = new EV3MediumRegulatedMotor(MotorPort.C);
         System.out.println("EV3-DeliveryTruck: Creating Motor D - Steering");
         final EV3MediumRegulatedMotor motorSteer = new EV3MediumRegulatedMotor(MotorPort.D);
+        System.out.println("Motor initialized");
 
         // Sensors
 
-        // final EV3TouchSensor touchLift = new EV3TouchSensor(SensorPort.S1);
-        // final EV3ColorSensor palletReader = new EV3ColorSensor(SensorPort.S3);
-        // final EV3ColorSensor lineReader = new EV3ColorSensor(SensorPort.S4);
+        System.out.println("EV3-DeliveryTruck: Creating Sensor S3 - Linereader");
+        final LineReaderV2 lineReader = new LineReaderV2(SensorPort.S3);
+        System.out.println("EV3-DeliveryTruck: Creating Sensor S1 - Left Camera");
+        final NXTCamV5 leftCam = new NXTCamV5(SensorPort.S1);
+        System.out.println("EV3-DeliveryTruck: Creating Sensor S4 - Right Camera");
+        final NXTCamV5 rightCam = new NXTCamV5(SensorPort.S4);
+        System.out.println("Sensors initialized");
 
         //To Stop the motor in case of pkill java for example
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
@@ -41,11 +46,15 @@ public class MyFirstRobot {
             }
         }));
 
+        //=======================================
+        /* DRIVING TEST
+        //=======================================
+
         System.out.println("Driving test");
         Delay.msDelay(1000);
 
-        motorDrive.setSpeed(300);
-        motorDrive.forward();
+        motorDrive.setSpeed(400);
+        motorDrive.backward();
 
         Delay.msDelay(2000);
 
@@ -64,178 +73,32 @@ public class MyFirstRobot {
 
         Delay.msDelay(2000);
 
-        motorDrive.backward();
+        motorDrive.forward();
 
         Delay.msDelay(2000);
 
         motorDrive.stop(true);
 
-        //System.out.println("EV3-Forklift: Defining the Stop mode");
-        //motorLift.brake();
-        /*motorTilt.brake();
-        motorDrive.brake();
-        motorSteer.brake();*/
-
-        /*SampleProvider palletSample = palletReader.getRGBMode();
-        int palletSampleSize = palletSample.sampleSize();
-        float[] palletSampleColor = new float[palletSampleSize];
-        palletSample.fetchSample(palletSampleColor, 0);
-
-        int palletColorSampleRed = (int) palletSampleColor[0];
-        int palletColorSampleGreen = (int) palletSampleColor[1];
-        int palletColorSampleBlue = (int) palletSampleColor[2];
-
-        float palletAverage = (palletColorSampleRed + palletColorSampleGreen + palletColorSampleBlue) / 3;
-
-        System.out.println("EV3-Forklift: palletReflect:" + palletAverage);
-
-        //lift pallet
-        System.out.println("EV3-Forklift: Lift up");
-        final int motorLiftSpeed = 300; //- for lifting up, + for lifting down
-        motorLift.setSpeed(motorLiftSpeed);
-        int liftAngle = -1500;
-        motorLift.rotateTo(liftAngle, true);
-        Delay.msDelay(3000);
-
-        //drive until red
-        final int motorSteerSpeed = 500; //+ for rotating to ?, - for rotating to right
-        motorSteer.setSpeed(motorSteerSpeed);
-
-        boolean stopFlag = false;
-        int count = 0;
-        int steerAngle;
-
-        System.out.println("EV3-Forklift: Drive forward");
-        final int motorDriveSpeed = 150; //+ for driving forward, - for driving backward
-        motorDrive.setSpeed(motorDriveSpeed);
-        motorDrive.forward();
-
-        while (!stopFlag) {
-            SampleProvider colorSample = lineReader.getRGBMode();
-            int sampleSizeColor = colorSample.sampleSize();
-            float[] sampleColor = new float[sampleSizeColor];
-            colorSample.fetchSample(sampleColor, 0);
-            int colorSampleRed = (int)sampleColor[0];
-            int colorSampleGreen = (int)sampleColor[1];
-            int colorSampleBlue = (int)sampleColor[2];
-
-            steerAngle = LineReaderS(colorSampleRed, colorSampleGreen, colorSampleBlue);
-            motorSteer.rotateTo(steerAngle, true);
-
-            System.out.println("EV3-Forklift: R:" + " steerAngle:" + steerAngle);
-
-
-            if (colorSampleRed > 300 && colorSampleGreen < 100 && colorSampleBlue < 100)
-            {
-                motorDrive.stop();
-                stopFlag = true;
-            }
-
-            count = count + 1;
-            if (count == 200) {
-                stopFlag = true;
-                motorDrive.stop();
-            }
-
-            Delay.msDelay(10);
-        }
-
-        motorSteer.rotateTo(0, true);
-
-        //lower pallet
-        liftAngle = -1000;
-        motorLift.rotateTo(liftAngle, false);
-        Delay.msDelay(2000);
-
-        //drive backward
-        motorDrive.backward();
-        Delay.msDelay(200);
-
-
-        //reset status
-        motorSteer.rotateTo(0, true);
-        motorLift.rotateTo(0, true);
-        Delay.msDelay(5000);
-
-        palletSample = palletReader.getRGBMode();
-        palletSampleSize = palletSample.sampleSize();
-        palletSampleColor = new float[palletSampleSize];
-        palletSample.fetchSample(palletSampleColor, 0);
-
-        palletColorSampleRed = (int) palletSampleColor[0];
-        palletColorSampleGreen = (int) palletSampleColor[1];
-        palletColorSampleBlue = (int) palletSampleColor[2];
-
-        palletAverage = (palletColorSampleRed + palletColorSampleGreen + palletColorSampleBlue) / 3;
-
-        System.out.println("EV3-Forklift: palletReflect:" + palletAverage);*/
-
-       /* final int motorSteerSpeed = 500; //+ for rotating to ?, - for rotating to right
-        motorSteer.setSpeed(motorSteerSpeed);
-        motorSteer.rotateTo(-300, true);
-        Delay.msDelay(2000);
-        motorSteer.rotateTo(0, true);
-        Delay.msDelay(2000); */
-
-        /*int[] steer = example.LineFollower.motorsSpeed(colorSampleRed, colorSampleGreen, colorSampleBlue);
-
-        //tilt up
-        /*System.out.println("EV3-Forklift: Tilt up");
-        final int motorTiltSpeed = 300; //+ for tilting down, - for tilting up
-        motorTilt.setSpeed(motorTiltSpeed);
-        motorTilt.backward();
-        Delay.msDelay(5000);
-        motorTilt.stop();
-
-        //lift up
-        System.out.println("EV3-Forklift: Lift up");
-        final int motorLiftSpeed = 300; //- for lifting up, + for lifting down
-        motorLift.setSpeed(motorLiftSpeed);
-        motorLift.backward();
-        Delay.msDelay(7000);
-        motorLift.stop();
-
-
-        //rotate steering
-        System.out.println("EV3-Forklift: Rotate Steering");
-        final int motorSteerSpeed = 500; //+ for rotating to ?, - for rotating to right
-        motorSteer.setSpeed(motorSteerSpeed);
-        motorSteer.forward();
-        Delay.msDelay(2000);
-        motorSteer.stop();
-
-        //drive forward
-        System.out.println("EV3-Forklift: Drive forward");
-        final int motorDriveSpeed = 300; //+ for driving forward, - for driving backward
-        motorDrive.setSpeed(motorDriveSpeed);
-        motorDrive.forward();
-        Delay.msDelay(2000);
-        motorDrive.stop();
-
-        //return steering
-        System.out.println("EV3-Forklift: Return steering");
-        motorSteer.setSpeed(motorSteerSpeed);
-        motorSteer.backward();
-        Delay.msDelay(2000);
-        motorSteer.stop();
-
-        //tilt down
-        System.out.println("EV3-Forklift: Tilt down");
-        motorTilt.setSpeed(motorTiltSpeed);
-        motorTilt.forward();
-        Delay.msDelay(5000);
-        motorTilt.stop();
-
-        //lift down
-        System.out.println("EV3-Forklift: Lift down");
-        motorLift.setSpeed(motorLiftSpeed);
-        motorLift.forward();
-        while (!touchLift.isPressed()) {
-            Delay.msDelay(100);
-        }
-        motorLift.stop();
-
         */
+
+        //=======================================
+        // Linereader Test
+        //=======================================
+
+        //lineReader.wake();
+
+        //for (int i = 0; i <= 40; i++){
+        //    System.out.println(lineReader.getPIDValue());
+        //    Delay.msDelay(1000);
+        //}
+
+        //lineReader.sleep();
+
+        //=======================================
+        // Camera Test
+        //=======================================
+
+
 
 
 
